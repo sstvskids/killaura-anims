de727d694f2ae7066c6d7e4849c186d668eb65f6
sstvskids <126665568+sstvskids@users.noreply.github.com>
2023-05-20 20:05:31 +1000

    Update 64472274481.lua

--- 64472274481.lua
+++ 64472274481.lua
@@ -4053,7 +4053,7 @@ runFunction(function()
     })
     killauraanimmethod = Killaura.CreateDropdown({
         Name = "Animation", 
-	List = {"Normal", "Slow", "New", "Fast", "Vertical Spin", "Exhibition", "Exhibition Old", "Custom", "SkidWare", "Sudicial", "Moonsoon"}
+	List = {"Normal", "Slow", "New", "Fast", "Vertical Spin", "Exhibition", "Exhibition Old", "Custom", "SkidWare", "Sudicial", "Moonsoon"},
         Function = function(val) end
     })
 	local oldviewmodel
